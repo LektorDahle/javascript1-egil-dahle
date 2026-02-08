eb9f68569a154266f898a44d44e65c8eb923424c
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-02-08 19:38:42 +0100

    Truthy did not work in the test
    ChatGPT suggested
    expect(logoImage).not.toBeNull();
    expect(logoImage).toBeDefined();
    to be used instead

--- script/header-footer.test.js
+++ script/header-footer.test.js
@@ -14,7 +14,7 @@ describe("checks the render of the header", () => {
     });
     test("header has a logo", () => {
         /**Tests that the addLogo does not throw any error */
-        expect(() => new Header(document.body).addLogo(".")).not.toThrow();
+        expect(() => new Header(document.body).addLogo("./img/icons/RD-logo-lightmode.svg")).not.toThrow();
 
         /** @type {HTMLElement | null} */
         const logoWrapper = document.querySelector(".logo-wrapper");
@@ -22,9 +22,15 @@ describe("checks the render of the header", () => {
 
         /** @type {HTMLElement | null | undefined} */
         const logoImage = logoWrapper?.querySelector("img");
-        expect(logoImage).not.toBeTruthy();
+        expect(logoImage).not.toBeNull();
+        expect(logoImage).toBeDefined();
 
+        /** @type {HTMLElement | null | undefined} */
         const logoLink = logoWrapper?.querySelector("a.logo");
         expect(logoLink?.getAttribute("href")).toBe("./");
+    });
+
+    test("add navigation link", () => {
+        expect(() => new Header(document.body).addLI(document.body, "./content", "Content")).not.toThrow();
     });
 });
