33de98336851ccaba609e73bb5c3f2a9c3d85177
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-02-13 00:41:34 +0100

    Some small updates to the spa-part

--- script/pages/home.js
+++ script/pages/home.js
@@ -1,5 +1,9 @@
-
 export function mainPage() {
+    window.location.hash = "mainpage"; // const screen = location.hash.slice(1); reads the url
     const main = document.getElementsByTagName("main")[0] || alert("Could not render page!");
     main.textContent = "Yo";
+}
+
+export class MainContentPage{
+
 }
---
+++ script/pages/pages.js
@@ -0,0 +1,6 @@
+export class Page {
+    constructor(location, ){
+    this.main = document.getElementsByTagName("main")[0] || alert("Could not render page!");
+    window.location.hash = location;}
+
+}
