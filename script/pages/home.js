a71fb35096360fdb5a96ac85f489504080f5b27e
Egil Johnsen Dahle <egil_dahle@hotmail.com>
2026-02-20 08:35:35 +0100

    Writing all the jsondata to the main page
    Next is to represent it better in "cards" and loading the images.

--- script/pages/home.js
+++ script/pages/home.js
@@ -1,10 +1,10 @@
-import {divider, paragraph, header} from "../html-elements.js";
+import { divider, paragraph } from "../html-elements.js";
 
 
 export function mainPage() {
     window.location.hash = "mainpage"; // const screen = location.hash.slice(1); reads the url
     const main = document.getElementsByTagName("main")[0] || alert("Could not render page!");
-    main.textContent = "Yo";
+    new MainContentPage(main);
 }
 
 export class MainContentPage {
@@ -12,19 +12,26 @@ export class MainContentPage {
      * 
      * @param {HTMLElement} main 
      */
-    constructor(main){
+    constructor(main) {
         this.main = main;
+        this.renderJackets();
+
+    }
+
+    async renderJackets() {
+        const allData = await this.getAllData();
+        this.main.innerHTML = JSON.stringify(allData);
     }
 
     async getAllData() {
         try {
             const res = await fetch("https://v2.api.noroff.dev/rainy-days");
-            if (!res.ok){
+            if (!res.ok) {
                 throw new Error(res.statusText);
             }
             return res.json();
         } catch (err) {
-            console.log(err);
+            this.showErrorOnScreen(err);
             throw err;
         }
     }
@@ -33,8 +40,8 @@ export class MainContentPage {
      * 
      * @param {*} err 
      */
-    showErrorOnScreen(err){
+    showErrorOnScreen(err) {
         const errorDiv = divider(this.main);
-        
+        paragraph(errorDiv, err);
     }
 }
